601053af4ac0a9db9764853f5b86ab0ad55c306e
hemantakumar.phurailatpam <hemantakumar.phurailatpam@ligo.org>
2023-05-25 18:56:05 +0800

    mac

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 setup(name='ler',
-      version='0.1.3',
+      version='0.1.4',
       description='Lensing Rates',
       author='Hemantakumar',
       license="MIT",
@@ -11,12 +11,15 @@ setup(name='ler',
       install_requires=[
         "setuptools>=61.1.0",
         "numpy>=1.18",
-        "bilby>=1.0.2",
-        "pycbc>=2.0.4",
-        "quintet>=0.1",
+        "numba==0.56.4",
+        "bilby>=1.0.2",  
+        "pycbc>=2.0.6",
+        "gwsnr>=0.1",
         "scipy>=1.9.0",
         "lenstronomy>=1.10.4",
         "astropy>=5.1",
-        "gwcosmo>=1.0.0",
+        "tqdm>=4.64.1",
+        "pointpats>=2.3",
+        "shapely>=2.0.1",
       ]
      )
