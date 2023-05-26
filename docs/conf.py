36baf781a3866fe13a83f4b1cec6adc69f5ea4a1
hemantakumar.phurailatpam <hemantakumar.phurailatpam@ligo.org>
2023-05-27 06:34:45 +0800

    mac

--- docs/conf.py
+++ docs/conf.py
@@ -1,21 +1,23 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# -- Path setup --------------------------------------------------------------
 
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
 import os
 import sys
-sys.path.insert(0, os.path.abspath("../..")) 
-
-import ler.ler
+sys.path.insert(0, os.path.abspath("..")) 
 
 project = 'ler'
-copyright = '2023, hemanta'
-author = 'hemanta'
-release = '1.0'
+copyright = '2023, Phurailatpam Hemantakumar'
+author = 'Phurailatpam Hemantakumar'
+release = '0.1.3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -29,12 +31,12 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.inheritance_diagram",
     "sphinx_tabs.tabs",
+    "autoapi.extension",
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.ipynb_checkpoints','.ipynb']
 numpydoc_show_class_members = False
-autodoc_member_order = 'bysource'
 
 
 
@@ -42,7 +44,12 @@ autodoc_member_order = 'bysource'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = []
+html_static_path = ['_static']
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ['_static']
+
+# -- Configure autoapi -------------------------------------------------------
+autoapi_type = "python"
+autoapi_dirs = ["../ler/"]
+autoapi_add_toctree_entry = False
+autoapi_options = ["members", "show-inheritance", "show-module-summary"]
