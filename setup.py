6d515a82645ca854bb55c96e315a1665368f17da
hemantaph <hemantaphurailatpam@gmail.com>
2023-06-19 23:39:09 +0800

    mac

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-setup(name='ler',
-      version='0.1.5',
-      description='Lensing Rates',
-      author='Hemantakumar',
-      license="MIT",
-      author_email='hemantaphurailatpam@gmail.com',
-      url='https://github.com/hemantaph/ler',
-      packages=find_packages(),
-      install_requires=[
+
+setup(
+    name="ler",
+    version="0.1.6",
+    description="Lensing Rates",
+    author="Hemantakumar",
+    license="MIT",
+    author_email="hemantaphurailatpam@gmail.com",
+    url="https://github.com/hemantaph/ler",
+    packages=find_packages(),
+    install_requires=[
         "setuptools>=67.8.0",
         "numpy>=1.18",
         "numba==0.56.4",
-        "bilby>=1.0.2",  
+        "bilby>=1.0.2",
         "pycbc<=2.0.6",
         "gwsnr>=0.1",
         "scipy>=1.9.0",
@@ -20,5 +22,6 @@ setup(name='ler',
         "astropy>=5.1",
         "tqdm>=4.64.1",
         "pointpats>=2.3",
-        "shapely>=2.0.1",]
-     )
+        "shapely>=2.0.1",
+    ],
+)
