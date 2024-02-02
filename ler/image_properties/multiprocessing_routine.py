ed76587bdb13ad4963ef5a5de3c2c374ec865d9e
Otto Akseli Hannuksela <otto.akseli.hannuksela@gmail.com>
2024-02-02 10:32:34 +0800

    Fixed lenstronomy solver routine

--- ler/image_properties/multiprocessing_routine.py
+++ ler/image_properties/multiprocessing_routine.py
@@ -139,7 +139,10 @@ def solve_lens_equation(lens_parameters):
                 sourcePos_y=y_source,
                 kwargs_lens=kwargs_lens,
                 solver="lenstronomy",
-                magnification_limit=1.0 / 1000.0,
+                min_distance=0.005, 
+                search_window=3, 
+                precision_limit=10**(-10), 
+                num_iter_max=100)
             )
             nImages = len(x0_image_position)  # shows how many images
             if nImages >= n_min_images:
